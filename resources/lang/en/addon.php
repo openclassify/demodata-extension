339584b8ca8716a532bc814a0c2b94ca005fa0b1
Mostafa Moradi <mostafa21tr@gmail.com>
2022-04-06 11:57:18 +0300

    initial

---
+++ resources/lang/en/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Demodata',
+    'name'        => 'Demodata Extension',
+    'description' => ''
+];
---
+++ tests/Feature/DemodataExtensionTest.php
@@ -0,0 +1,10 @@
+<?php namespace Visiosoft\DemodataExtension\Test\Feature;
+
+class DemodataExtensionTest extends \TestCase
+{
+
+    public function testFeature()
+    {
+        $this->markTestSkipped('Not implemented.');
+    }
+}
