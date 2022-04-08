f641cd531b7ddd86f2b5370c15b3699cf21b4989
Mostafa Moradi <mostafa21tr@gmail.com>
2022-04-08 16:10:55 +0300

    Database seeder

--- src/DemodataExtensionSeeder.php
+++ src/DemodataExtensionSeeder.php
@@ -15,11 +15,9 @@ class DemodataExtensionSeeder extends Seeder
         $application_reference = (new ArgvInput())->getParameterOption('--app', env('APPLICATION_REFERENCE', 'default'));
         $categories = str_replace('{application_reference}', $application_reference, file_get_contents(realpath(dirname(__DIR__)) . '/src/seed/data/categories.sql'));
         $advs = str_replace('{application_reference}', $application_reference, file_get_contents(realpath(dirname(__DIR__)) . '/src/seed/data/advs.sql'));
-        $settings = str_replace('{application_reference}', $application_reference, file_get_contents(realpath(dirname(__DIR__)) . '/src/seed/data/settings.sql'));
         Model::unguard();
         DB::unprepared($advs);
         DB::unprepared($categories);
-        DB::unprepared($settings);
         Model::reguard();
         $zip = new \ZipArchive();
         $zip->open(realpath(dirname(__DIR__)) . '/src/seed/data/images.zip', ZipArchive::CREATE);
