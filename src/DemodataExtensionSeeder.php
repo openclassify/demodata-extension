3cd160444d5bc2edb3c563ff57aed830a7c48752
Vedat AKDOĞAN <39536659+spektra2147@users.noreply.github.com>
2022-07-28 12:35:11 +0300

    update path

--- src/DemodataExtensionSeeder.php
+++ src/DemodataExtensionSeeder.php
@@ -13,16 +13,16 @@ class DemodataExtensionSeeder extends Seeder
     public function run()
     {
         $application_reference = (new ArgvInput())->getParameterOption('--app', env('APPLICATION_REFERENCE', 'default'));
-        $categories = str_replace('{application_reference}', $application_reference, file_get_contents(realpath(dirname(__DIR__)) . '/src/seed/data/categories.sql'));
-        $advs = str_replace('{application_reference}', $application_reference, file_get_contents(realpath(dirname(__DIR__)) . '/src/seed/data/advs.sql'));
+        $categories = str_replace('{application_reference}', $application_reference, file_get_contents(realpath(dirname(__DIR__)) . '/src/Seed/Data/categories.sql'));
+        $advs = str_replace('{application_reference}', $application_reference, file_get_contents(realpath(dirname(__DIR__)) . '/src/Seed/Data/advs.sql'));
         Model::unguard();
         DB::unprepared($advs);
         DB::unprepared($categories);
         Model::reguard();
         $zip = new \ZipArchive();
-        $zip->open(realpath(dirname(__DIR__)) . '/src/seed/data/images.zip', ZipArchive::CREATE);
+        $zip->open(realpath(dirname(__DIR__)) . '/src/Seed/Data/images.zip', ZipArchive::CREATE);
         $zip->extractTo(storage_path('streams/' . $application_reference . '/files-module/local/images/'));
-        $zip->open(realpath(dirname(__DIR__)) . '/src/seed/data/cats.zip', ZipArchive::CREATE);
+        $zip->open(realpath(dirname(__DIR__)) . '/src/Seed/Data/cats.zip', ZipArchive::CREATE);
         $zip->extractTo(storage_path('streams/' . $application_reference . '/files-module/local/category_icon/'));
         $zip->close();
         //Sync Files
