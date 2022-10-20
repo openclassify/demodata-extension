b5c2fd8498b3af50732e0340e039f9017c5c5afd
erhanask <erhanasik8@gmail.com>
2022-10-20 11:29:59 +0300

    Added detailed category demodatas from demodata-module.

--- src/DemodataExtensionSeeder.php
+++ src/DemodataExtensionSeeder.php
@@ -14,10 +14,12 @@ class DemodataExtensionSeeder extends Seeder
     {
         $application_reference = (new ArgvInput())->getParameterOption('--app', env('APPLICATION_REFERENCE', 'default'));
         $categories = str_replace('{application_reference}', $application_reference, file_get_contents(realpath(dirname(__DIR__)) . '/src/Seed/Data/categories.sql'));
+        $categoriesTrans = str_replace('{application_reference}', $application_reference, file_get_contents(realpath(dirname(__DIR__)) . '/src/Seed/Data/categoryTransEn.sql'));
         $advs = str_replace('{application_reference}', $application_reference, file_get_contents(realpath(dirname(__DIR__)) . '/src/Seed/Data/advs.sql'));
         Model::unguard();
         DB::unprepared($advs);
         DB::unprepared($categories);
+        DB::unprepared($categoriesTrans);
         Model::reguard();
         $zip = new \ZipArchive();
         $zip->open(realpath(dirname(__DIR__)) . '/src/Seed/Data/images.zip', ZipArchive::CREATE);
